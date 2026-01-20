b9dbc4154fd1beef960a8db5d4e203d55d6fb39f
Roopa K S <ksroopa04@gmail.com>
2026-01-20 17:00:51 +0530

    testttt

---
+++ Roopa/src/Client Development/Client Scripts/test.script.js
@@ -0,0 +1,8 @@
+function onChange(control, oldValue, newValue, isLoading, isTemplate) {
+   if (isLoading || newValue === '') {
+      return;
+   }
+
+   //Type appropriate comment here, and begin script below
+   //vscode....
+}
---
+++ consiliosandbox/x_nsllc_sf_process/sys_script_client/AutoPopulated.script.js
@@ -0,0 +1,15 @@
+function onChange(control, oldValue, newValue, isLoading) {
+   if (isLoading || !newValue) {
+       return;
+   }
+   // Get reference field values using callback
+   g_form.getReference('project_hosting_infrastructure', function(ref) {
+       if (!ref) {
+           return;
+       }
+       g_form.setValue('review_platform', ref.u_review_platform);
+       g_form.setValue('review_platform_url', ref.u_review_platform_url);
+       g_form.setValue('data_center_reference', ref.u_data_center_reference);
+       //...
+   });
+}
